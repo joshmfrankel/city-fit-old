bae26152216a136c7954e845f000e23478b9e1fd
Josh Frankel <joshmfrankel@gmail.com>
2015-02-04 22:44:22 -0500

    Use full_title for assert_select on title element

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  def setup
-    @base_title = "CityFit: Solving couples two body problem"
-  end
 
   test "should get new" do
     get :new
     assert_response :success
-    assert_select 'title', "Sign Up | #{@base_title}"
+    assert_select 'title', full_title("Sign Up")
   end
 
 end
