de022295cc456ac4826ab9fa90f8bb902198639a
Josh Frankel <joshmfrankel@gmail.com>
2015-02-04 22:44:47 -0500

    Add signup link, add signup path

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'about'   => 'pages#about'
   get 'contact' => 'pages#contact'
 
-  # Users
+    # Users
   get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
