5abf82658e53c78156de1294bb8b7e55283123de
Josh Frankel <joshmfrankel@gmail.com>
2015-02-04 22:23:04 -0500

    Add contact, about, home; Add helper for <title>, Add integration test for site layout and navigation, Add title checking tests, Add setup block for page test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def full_title(active_title = '')
+    base_title = "CityFit: Solving couples two body problem"
+    if active_title.empty?
+      # return the base title essentially
+      base_title
+    else
+      # return the concatentation
+      "#{active_title} | #{base_title}"
+    end
+  end
 end
