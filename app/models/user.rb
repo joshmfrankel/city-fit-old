b953377b579098df5dc471d7d0111d82672bbea6
Josh Frankel <joshmfrankel@gmail.com>
2015-02-05 23:00:29 -0500

    Basic validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
 
+  before_save { email.downcase! }
+
   # Force email addresses to be lowercase
   before_save { self.email = email.downcase }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
@@ -12,4 +14,12 @@ class User < ActiveRecord::Base
   # Two virtual attributes (password and password_confirmation) + presence validations
   # Auth method build in
   has_secure_password
+
+  # http://www.zorched.net/2009/05/08/password-strength-validation-with-regular-expressions/
+  # Zero-width positive lookahead assertions
+  # @todo Need test for this
+  VALID_PASSWORD_REGEX = /(?=.*[a-z])(?=.*[\d])(?=.*[\W])(?=.*[A-Z])/
+
+  # Virtual Model Attributes from has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: VALID_PASSWORD_REGEX }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,7 +12,7 @@ class UserTest < ActiveSupport::TestCase
 
   # Pre-runner
   def setup
-    @user = User.new(name: 'Josh', email: 'joshmfrankel@gmail.com', password: 'foobar', password_confirmation: 'foobar')
+    @user = User.new(name: 'Josh', email: 'joshmfrankel@gmail.com', password: 'ArealyF#ingWork1ngPassowrd', password_confirmation: 'ArealyF#ingWork1ngPassowrd')
   end
 
   # Validations
@@ -52,8 +52,8 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w(user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz..com)
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -65,6 +65,18 @@ class UserTest < ActiveSupport::TestCase
     dup_user.email = @user.email.upcase
     @user.save
     assert_not dup_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+     mixed_case_email = "Foo@ExAMPle.CoM"
+     @user.email = mixed_case_email
+     @user.save
+     assert_equal mixed_case_email.downcase, @user.reload.email
+   end
+
+  test 'password should not be too short' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
   end
 
 end
