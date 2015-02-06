af9b64a8ec07db54e193167d61eb6268e915b7ee
Josh Frankel <joshmfrankel@gmail.com>
2015-02-05 23:12:37 -0500

    Add restful routes, Add sass and sass directory organization

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
     # Users
   get 'signup' => 'users#new'
+  resources :users # adds restful routes
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
